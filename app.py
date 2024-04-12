71e653367bf04ad15148ad5afcb416450e98ecd4
uuyhd <uuyhd@student.kit.edu>
2024-04-12 12:51:45 +0200

    backtracking

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     sudoku_board = Board()
     sudoku_board.generateRandomStart()
     sudoku_board.print_board()
+    if sudoku_board.solve():
+        sudoku_board.print_board()
+    else:
+        print("not solvable")
--- board.py
+++ board.py
@@ -50,3 +50,37 @@ class Board:
                     return False
 
         return True
+
+    def solve(self):  # back tracking algorithm
+        # find all free cells
+        freeCells = []
+        for i in range(0, 81):
+            row = i//9
+            col = i % 9
+            if self.grid[row][col] == ".":
+                freeCells.append((row, col))
+        print(freeCells)
+
+        index = 0
+        found = False
+        while index < len(freeCells):
+            row, col = freeCells[index]
+            num = self.grid[row][col]
+
+            if num == ".":
+                num = 0  # empty, start trying from 1
+            found = False
+
+            for n in range(num+1, 10):
+                if self.validateCell(n, row, col):
+                    self.grid[row][col] = n
+                    index += 1  # Move to the next cell
+                    found = True
+                    break
+
+            if not found:  # If no valid number was found, backtrack
+                if index == 0:
+                    return False  # Unsolvable
+                self.grid[row][col] = "."
+                index -= 1
+        return True
